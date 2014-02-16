d356164d0860fe0a3ddaeb515c0dc8e6975a344e
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-02-16 14:06:34 -0500

    Changed page layout for User Dashboard

--- app/controllers/liusers_controller.rb
+++ app/controllers/liusers_controller.rb
@@ -1,8 +1,10 @@
 class LiusersController < ApplicationController
   
   def create
-    @code = params[:code]
-    get_contacts
+    if params[:code].nil?
+      get_auth
+    end 
+    get_token
     redirect_to(:back)
   end   
 
@@ -12,28 +14,31 @@ class LiusersController < ApplicationController
   end 
 
   private
-  def get_linkedin
-    consumer_key = LI_CONSUMER_KEY
-    consumer_secret = LI_CONSUMER_SECRET
-    token = LI_TOKEN
-    token_secret = LI_TOKEN_SECRET
+  ### Previously used with no authentication. 
+  # def get_linkedin
+  #   consumer_key = LI_CONSUMER_KEY
+  #   consumer_secret = LI_CONSUMER_SECRET
+  #   token = LI_TOKEN
+  #   token_secret = LI_TOKEN_SECRET
     
-    api_host = 'api.linkedin.com'
+  #   api_host = 'api.linkedin.com'
   
-    consumer = ::OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
-    access_token = ::OAuth::AccessToken.new(consumer, token, token_secret)
+  #   consumer = ::OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
+  #   access_token = ::OAuth::AccessToken.new(consumer, token, token_secret)
     
-    path = "/v1/people/~/connections?format=json"
-    @response = JSON(access_token.get(path).body)
-    @response = @response["values"]
-    
-  end
+  #   path = "/v1/people/~/connections?format=json"
+  #   @response = JSON(access_token.get(path).body)
+  #   @response = @response["values"]
+  # end
 
-  def get_contacts
-    consumer_key = LI_CONSUMER_KEY
-    consumer_secret = LI_CONSUMER_SECRET
-    @token = HTTParty.get("https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code&code=#{params[:code]}&redirect_uri=http://localhost:3000/users/#{session[:user_id]}/lilist&client_id=#{consumer_key}&client_secret=#{consumer_secret}")
-    binding.pry
+  def get_auth
+    @consumer_key = LI_CONSUMER_KEY
+    @consumer_secret = LI_CONSUMER_SECRET
+    redirect_to("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=<%=consumer_key%>&state=DCEEFWF45453sdffef424&redirect_uri=http://localhost:3000/users/<%=params[:id]%>/lilist") and return 
+  end 
+
+  def get_token
+    @token = HTTParty.get("https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code&code=#{params[:code]}&redirect_uri=http://localhost:3000/users/#{session[:user_id]}/lilist&client_id=#{@consumer_key}&client_secret=#{@consumer_secret}")
   end 
   def add_contacts
     # @contacts.each do |response|
