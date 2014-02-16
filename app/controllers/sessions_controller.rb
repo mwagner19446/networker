1d5ee2e8997655f4f322099ec0d0d7563dd7ee79
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-02-16 10:52:50 -0500

    Changed spacing on Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,8 @@
 class SessionsController < ApplicationController
   def new
   end 
+  
   def create
-  
-    ##authenticate:  check whether this information matches what is in the database
-    ##If so, go to the user page...
-    ##If not.. Destroy
-  
     user = User.find_by(email: params[:email])
     if user && (user.password == (params[:password]) )
       session[:user_id] = user.id
@@ -14,10 +10,9 @@ class SessionsController < ApplicationController
     else
       @message = "This email and password combination does not exist"
       render(:new)
-
     end 
-  
   end 
+  
   def destroy
       session[:user_id] = nil
       redirect_to("/")
