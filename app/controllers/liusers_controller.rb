ac8c7badc40f98d32e491622190f445878eb0024
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-02-18 11:24:06 -0500

    Continued work on Linked IN Auth... Read Me file updated

--- app/controllers/liusers_controller.rb
+++ app/controllers/liusers_controller.rb
@@ -1,20 +1,18 @@
 class LiusersController < ApplicationController
   
   def create
-    if params[:code].nil?
-      get_auth
-    end 
-    get_token
+    
+    get_token  
     redirect_to(:back)
   end   
 
   def list
-  if session[:connections].nil?
-    session[:connections] = []
-  end 
-  @liadds = Liuser.where(id: session[:connections])
-  @liusers = Liuser.all
-  render(:list)
+    if session[:connections].nil?
+      session[:connections] = []
+    end 
+    @liadds = Liuser.where(id: session[:connections])
+    @liusers = Liuser.all
+    render(:list)
   end 
 
   def connections
@@ -38,16 +36,19 @@ class LiusersController < ApplicationController
   #   path = "/v1/people/~/connections?format=json"
   #   @response = JSON(access_token.get(path).body)
   #   @response = @response["values"]
-  # end
+  # end 
 
   def get_auth
     @consumer_key = LI_CONSUMER_KEY
     @consumer_secret = LI_CONSUMER_SECRET
-    redirect_to("https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=<%=consumer_key%>&state=DCEEFWF45453sdffef424&redirect_uri=http://localhost:3000/users/<%=params[:id]%>/lilist") and return 
+    "https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=#{@consumer_key}%>&state=DCEEFWF45453sdffef424&redirect_uri=http://localhost:3000/users/#{session[:user_id]}/lilist"
   end 
 
   def get_token
-    @token = HTTParty.get("https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code&code=#{params[:code]}&redirect_uri=http://localhost:3000/users/#{session[:user_id]}/lilist&client_id=#{@consumer_key}&client_secret=#{@consumer_secret}")
+    session[:code]=params[:code]
+    binding.pry
+
+    @token = HTTParty.get("https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code&code=#{session[:code]}&redirect_uri=http://localhost:3000/users/#{session[:user_id]}/lilist&client_id=#{@consumer_key}&client_secret=#{@consumer_secret}")
   end 
   def add_contacts
     # @contacts.each do |response|
@@ -58,6 +59,5 @@ class LiusersController < ApplicationController
     # end 
 
   end 
-
 
 end 
