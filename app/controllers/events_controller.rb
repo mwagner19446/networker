8367bb257c53322cc4db85497a0c2e75f03bad55
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-02-18 16:33:05 -0500

    Updated Meetup Events to be more restful

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,14 @@
+class EventsController < ApplicationController
+  def index
+  end 
+
+  def search
+    zipcode = params[:zipcode]
+    topic = params[:topic]
+    topic = topic.gsub(" ","+")
+    meetup_api = MEETUP_API    
+    @event_meetup = HTTParty.get("http://api.meetup.com/2/open_events?status=upcoming&radius=25.0&and_text=False&limited_events=False&text=#{topic}&desc=False&offset=0&format=json&zip=#{zipcode}&page=20&key=#{meetup_api}")
+    @event_meetup = @event_meetup["results"]
+  end 
+
+end 
