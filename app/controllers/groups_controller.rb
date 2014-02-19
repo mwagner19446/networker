a910f9e04741c525299c1cf3b8c96cb80f443390
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-02-19 10:16:01 -0500

    Updated Meetup Groups Page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,7 +12,7 @@ class GroupsController < ApplicationController
   def search
     zipcode = params[:zipcode]
     topic = params[:topic]
-    topic = topic.gsub(" ","+")
+    # topic = topic.gsub(" ","+")
     meetup_api = MEETUP_API    
     @group_meetup = HTTParty.get("http://api.meetup.com/groups.json/?zip=#{zipcode}&text=#{topic}&order=members&key=#{meetup_api}")
     @group_meetup = @group_meetup["results"]
