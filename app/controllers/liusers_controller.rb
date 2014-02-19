e55a9d28e112630cc13a5e0edcb0de170ed3f345
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-02-18 19:09:41 -0500

    Change Back to the Linked In API

--- app/controllers/liusers_controller.rb
+++ app/controllers/liusers_controller.rb
@@ -1,6 +1,6 @@
 class LiusersController < ApplicationController
   
-  # before_action(:get_linkedin, {only: [:list] })
+  before_action(:get_linkedin, {only: [:list] })
 
   def create
   end   
@@ -44,22 +44,21 @@ class LiusersController < ApplicationController
 
 
   ### Previously used with no authentication. 
-  # def get_linkedin
-  #   consumer_key = LI_CONSUMER_KEY
-  #   consumer_secret = LI_CONSUMER_SECRET
-  #   token = LI_TOKEN
-  #   token_secret = LI_TOKEN_SECRET
+  def get_linkedin
+    consumer_key = LI_CONSUMER_KEY
+    consumer_secret = LI_CONSUMER_SECRET
+    token = LI_TOKEN
+    token_secret = LI_TOKEN_SECRET
     
-  #   api_host = 'api.linkedin.com'
+    api_host = 'api.linkedin.com'
   
-  #   consumer = ::OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
-  #   access_token = ::OAuth::AccessToken.new(consumer, token, token_secret)
+    consumer = ::OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
+    access_token = ::OAuth::AccessToken.new(consumer, token, token_secret)
     
-  #   path = "/v1/people/~/connections?format=json"
-  #   @response = JSON(access_token.get(path).body)
-  #   @response = @response["values"]
-  #   binding.pry
-  # end 
+    path = "/v1/people/~/connections?format=json"
+    @response = JSON(access_token.get(path).body)
+    @response = @response["values"]
+  end 
 
    
   def add_contacts
