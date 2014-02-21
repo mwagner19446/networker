2157127cc0784e0cee74d5534c4685b2cb846703
Michael K. Wagner <michael.k.wagner@gmail.com>
2014-02-21 08:50:34 -0500

    Update the Liuser Controller

--- app/controllers/liusers_controller.rb
+++ app/controllers/liusers_controller.rb
@@ -1,21 +1,12 @@
 class LiusersController < ApplicationController
-  
-  # before_action(:get_linkedin, {only: [:list] })
-
-  # before_action(:user_linkedin, {only: [:list] })
 
   before_action(:get_token, {only: [:list] })
-
 
   def index
   end 
 
   def create
   end   
-
-  def list
-    # @response = Liuser.all
-  end 
 
   def search
     get_token
@@ -25,30 +16,6 @@ class LiusersController < ApplicationController
   end 
 
   private
-
-  # def get_linkedin
-  #   access_token = Token.find_by(user_id: params[:user_id])
-  #   path = "/v1/people/~/connections?format=json"
-  #   @response = JSON(access_token.get(path).body)
-  #   @response = @response["values"]
-
-  # end
-
-  #   def get_linkedin
-  #   consumer_key = LI_CONSUMER_KEY
-  #   consumer_secret = LI_CONSUMER_SECRET
-  #   token = LI_TOKEN
-  #   token_secret = LI_TOKEN_SECRET
-    
-  #   api_host = 'api.linkedin.com'
-  
-  #   consumer = ::OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
-  #   access_token = Token.find_by(user_id: params[:user_id])
-    
-  #   path = "/v1/people/~/connections?format=json"
-  #   @response = JSON(access_token.get(path).body)
-  #   @response = @response["values"]
-  # end 
 
   def get_token
     @token = Token.where(user_id: params[:user_id])
@@ -63,34 +30,4 @@ class LiusersController < ApplicationController
     @response = @users["values"]
     render(:list)
   end 
-
-
-  ### Previously used with no authentication. 
-  def get_linkedin
-    consumer_key = LI_CONSUMER_KEY
-    consumer_secret = LI_CONSUMER_SECRET
-    token = LI_TOKEN
-    token_secret = LI_TOKEN_SECRET
-    
-    api_host = 'api.linkedin.com'
-  
-    consumer = ::OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
-    access_token = ::OAuth::AccessToken.new(consumer, token, token_secret)
-    
-    path = "/v1/people/~/connections?format=json"
-    @response = JSON(access_token.get(path).body)
-    @response = @response["values"]
-  end 
-
-   
-  def add_contacts
-    # @contacts.each do |response|
-    #   Liuser.create(:linkedin_id response["id"], :first_name response["firstName"], :last_name response["lastName"], 
-    #     :headline response["headline"], 
-    #     :industry response["industry"], 
-    #     :picture_url response["pictureUrl"])
-    # end 
-
-  end 
-
 end 
